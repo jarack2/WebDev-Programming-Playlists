901ae29b6d276f647a8269be21d3fe4b136d18d1
jared <jared.a.rackley@gmail.com>
2020-09-29 02:07:07 -0700

    feedback now posts

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -9,9 +9,11 @@
   <?php require_once "../structure/structure.php"; ?>
   <div class="main">
     <div class="content">
-      <h3 class="feedback-title">Submit Your Feedback Here!</h3>
-      <textarea name="Feedback" id="" cols="75" rows="10"></textarea>
-      <input type="submit" value="Submit">
+      <form action="/" method="post">
+        <h3 class="feedback-title">Submit Your Feedback Here!</h3>
+        <textarea name="Feedback" id="" cols="75" rows="10"></textarea>
+        <input type="submit" value="Submit">
+      </form>
     </div>
   </div>
   <?php require_once "../footer/footer.php"; ?>
