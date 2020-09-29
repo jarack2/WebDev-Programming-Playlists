d41c238a8fcae48e9e372d435e5a8244783e16fe
jared <jared.a.rackley@gmail.com>
2020-09-28 21:13:08 -0700

    sample video

--- playlists/playlists.php
+++ playlists/playlists.php
@@ -2,14 +2,14 @@
 
 <head>
   <link rel="stylesheet" href="../styles.css">
-  <link rel="stylesheet" href="playlists.css">
+  <link rel="stylesheet" href="playlists/playlists.css">
 </head>
 
 <body>
   <?php require_once "../structure/structure.php"; ?>
   <div class="main">
     <div class="videos">
-      <iframe width="380" height="225" src="https://www.youtube.com/embed/GhQdlIFylQ8" frameborder="0"
+      <iframe class="videos" width="380" height="225" src="https://www.youtube.com/embed/GhQdlIFylQ8" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen></iframe>
     </div>
