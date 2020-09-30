197af9c04dc161f7e01f6b268e5d60c47e4f8040
jared <jared.a.rackley@gmail.com>
2020-09-29 19:57:16 -0700

    placeholder videos and styling

--- playlists/playlists.php
+++ playlists/playlists.php
@@ -3,17 +3,27 @@
 <head>
   <link rel="stylesheet" href="../styles.css">
   <link rel="stylesheet" href="playlists/playlists.css">
-  <title>Programming Playlists</title>
+  <title id="home">Programming Playlists</title>
 </head>
 
 <body>
   <?php require_once "../structure/structure.php"; ?>
   <div class="main">
+    <!-- These will be dynamically generated but for now, they are hardcoded in -->
+    <h3 class="selectedplaylist">The Selected Playlist is: C#</h3>
+    <?php for ($x = 0; $x <= 3; $x++) { ?>
     <div class="videos">
       <iframe class="videos" width="380" height="225" src="https://www.youtube.com/embed/GhQdlIFylQ8" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen></iframe>
+      <iframe class="videos" width="380" height="225" src="https://www.youtube.com/embed/GhQdlIFylQ8" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+      <iframe class="videos" width="380" height="225" src="https://www.youtube.com/embed/GhQdlIFylQ8" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
     </div>
+    <?php } ?>
   </div>
 </body>
 <?php require_once "../structure/footer.php"; ?>
