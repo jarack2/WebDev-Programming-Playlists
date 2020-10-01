76df6ef300fa49f0c24c83c4044e43354109da4a
jared <jared.a.rackley@gmail.com>
2020-10-01 11:01:25 -0700

    small resizing changes for the videos

--- playlists/playlists.php
+++ playlists/playlists.php
@@ -15,17 +15,11 @@
     <h3 class="selectedplaylist">The Selected Playlist is: C#</h3>
     <!-- Hope you like C# -->
     <?php for ($x = 0; $x <= 3; $x++) { ?>
-    <div class="videos">
-      <iframe class="videos" width="380" height="225" src="https://www.youtube.com/embed/GhQdlIFylQ8" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen></iframe>
-      <iframe class="videos" width="380" height="225" src="https://www.youtube.com/embed/GhQdlIFylQ8" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen></iframe>
-      <iframe class="videos" width="380" height="225" src="https://www.youtube.com/embed/GhQdlIFylQ8" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen></iframe>
-    </div>
+      <div class="videos">
+        <iframe class="videos" src="https://www.youtube.com/embed/GhQdlIFylQ8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="videos" src="https://www.youtube.com/embed/GhQdlIFylQ8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="videos" src="https://www.youtube.com/embed/GhQdlIFylQ8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
     <?php } ?>
   </div>
 </body>
