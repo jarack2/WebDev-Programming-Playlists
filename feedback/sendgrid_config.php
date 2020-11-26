805173d20546dc54daeb49497fec2478fd2339e0
Jared <jared.a.rackley@gmail.com>
2020-11-25 20:53:08 -0800

    unfortunately we are adding the sendgrid key to heroku

---
+++ feedback/sendgrid_config.php
@@ -0,0 +1,3 @@
+<?php
+// this would normally be a private file, but I cannot out how to host it on heroku without pushing it, so RIP
+$sendgrid_apikey = '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';
