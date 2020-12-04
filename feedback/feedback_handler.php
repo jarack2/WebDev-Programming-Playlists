3161cdca367f139953317957d6dc1c9b86c82f64
Jared <jared.a.rackley@gmail.com>
2020-12-03 16:48:51 -0800

    adding error handling

--- feedback/feedback_handler.php
+++ feedback/feedback_handler.php
@@ -8,19 +8,28 @@ if (isset($_SESSION["username"])) {
   $name = $_SESSION["username"];
 }
 
-$from = new SendGrid\Email(null, "jaredrackley@u.boisestate.edu");
-$subject = "Feedback from $name";
-$to = new SendGrid\Email(null, "jaredrackley@u.boisestate.edu");
-$content = new SendGrid\Content("text/plain", $_POST["feedback"]);
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
+try {
 
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
+  $from = new SendGrid\Email(null, "jaredrackley@u.boisestate.edu");
+  $subject = "Feedback from $name";
+  $to = new SendGrid\Email(null, "jaredrackley@u.boisestate.edu");
+  $content = new SendGrid\Content("text/plain", $_POST["feedback"]);
+  $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->body();
+  $apiKey = getenv('SENDGRID_API_KEY');
+  $sg = new \SendGrid($apiKey);
 
+  $response = $sg->client->mail()->send()->post($mail);
+  echo $response->statusCode();
+  echo $response->body();
+} catch (Exception $e) {
+  echo $e;
+  if ($heroku) {
+    header("Location:https://programmingplaylists.herokuapp.com/feedback/feedback.php");
+  } else {
+    header("Location:http://cs401fp/feedback/feedback.php");
+  }
+}
 if (isset($_POST["feedback"])) {
   if (acceptable($response)) {
     $_SESSION["feedback_submitted"] = true;
